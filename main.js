d874952ce7f0c5e52df8a92d82b0376ded983184
Alexsander P.F. Santos <82063944+Alexsander33set@users.noreply.github.com>
2024-06-12 01:20:10 -0300

    (main.js) Electron screen changes

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ import { app, BrowserWindow } from 'electron'
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 960,
-    height: 720
+    height: 720,
+    autoHideMenuBar: true,
+    center:true,
+    icon:'./dist/favicon.ico'
   })
 
   win.loadFile('./dist/index.html')
